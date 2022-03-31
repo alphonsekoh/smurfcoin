8430573ba6ff696fed2d9f8c4882480bf1d9721a
Huan Yin Koh <alphonsekoh@gmail.com>
2022-03-31 18:22:50 +0800

    task 2 a-c done

--- app/src/main/java/edu/singaporetech/btco/BTCOActivity.kt
+++ app/src/main/java/edu/singaporetech/btco/BTCOActivity.kt
@@ -22,6 +22,43 @@ class BTCOActivity : AppCompatActivity(), CoroutineScope by MainScope() {
         super.onCreate(savedInstanceState)
         binding = ActivityLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.genesisButton.setOnClickListener {
+            emptyLogs()
+        }
+
+        binding.chainButton.setOnClickListener {
+            emptyLogs()
+            // if transaction message is empty
+            if(binding.msgEditText.text.isEmpty()){
+                binding.logTextView.append("describe your transaction in words...\n")
+            }
+
+            // if block number is empty
+            if(binding.blocksEditText.text.isEmpty()){
+                binding.logTextView.append("blocks cannot be empty...\n")
+            }
+
+            // if block number is out of range
+            if(binding.blocksEditText.text.toString().toInt() < 2 || binding.blocksEditText.text.toString().toInt() > 888){
+                binding.logTextView.append("blocks must be 2 to 888...\n")
+            }
+        }
     }
+
+    /**
+     *  If text is empty show error on Logs
+     */
+    private fun emptyLogs(){
+        // If difficulty edit text is empty
+        if(binding.difficultyEditText.text.isEmpty()){
+            binding.logTextView.append("difficulty is empty...\n")
+        }
+        // If difficulty edit text is out of range
+        if(binding.difficultyEditText.text.toString().toInt() < 1 || binding.difficultyEditText.text.toString().toInt() > 10){
+            binding.logTextView.append("difficulty must be 1 to 10...\n")
+        }
+    }
+
 
 }
